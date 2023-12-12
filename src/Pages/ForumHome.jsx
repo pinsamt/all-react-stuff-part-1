cc6070094cd4c13adc428afcc6113535674d84db
pinsamt <felix.vallejo99v@gmail.com>
2023-12-12 09:38:43 +0100

    dada

--- src/Pages/ForumHome.jsx
+++ src/Pages/ForumHome.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 const ForumHome = (props) => {
   return (
